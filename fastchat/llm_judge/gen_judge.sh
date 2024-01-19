177c4f041b4e4fd6cab58f53d8ec66abd393f4ff
Ubuntu <ken@braingptbenchmark2.y1aclwlxrr5exhycxfsi0tfn1e.fx.internal.cloudapp.net>
2024-01-19 21:38:57 +0000

    complete three steps

--- fastchat/llm_judge/gen_judge.sh
+++ fastchat/llm_judge/gen_judge.sh
@@ -1,7 +1,17 @@
+python gen_model_answer.py \
+    --model-path /datadrive1/finetuned_models/finetune_llama2_chat_7b_Attention/checkpoint.0 \
+    --model-id finetune_llama2_chat_7b_Attention \
+    # --num-gpus-total 4
+
 python gen_judgment.py \
+    --model-list \
+        finetune_llama2_chat_7b_Attention \
+    --parallel 3 \
+    --judge-model gpt-4
+
+python show_result.py \
     --model-list \
         llama2_chat_7b \
         finetune_llama2_chat_7b \
         finetune_llama2_chat_7b_FFNet \
-    --parallel 3 \
-    --judge-model gpt-4
+        finetune_llama2_chat_7b_Attention
