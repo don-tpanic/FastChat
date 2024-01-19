40e5632c651eb734555c8a3e2c7cd31a8ded5396
Ubuntu <ken@braingptbenchmark2.y1aclwlxrr5exhycxfsi0tfn1e.fx.internal.cloudapp.net>
2024-01-19 18:57:08 +0000

    add code: quick judge launch

---
+++ fastchat/llm_judge/gen_judge.sh
@@ -0,0 +1,7 @@
+python gen_judgment.py \
+    --model-list \
+        llama2_chat_7b \
+        finetune_llama2_chat_7b \
+        finetune_llama2_chat_7b_FFNet \
+    --parallel 3 \
+    --judge-model gpt-4
